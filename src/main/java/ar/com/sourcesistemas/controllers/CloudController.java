681cb5236b5f1776b6a6f32a48eed81153740c6c
ih4t3youall <ih4t3youall@gmail.com>
2018-11-17 16:05:30 -0300

    log4j working

--- src/main/java/ar/com/sourcesistemas/controllers/CloudController.java
+++ src/main/java/ar/com/sourcesistemas/controllers/CloudController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.apache.log4j.Logger;
 @RestController
 public class CloudController {
 
+    final static Logger log = Logger.getLogger(CloudController.class);
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test(String name){
+        log.info("hola mundo del log");
         return "hello "+name;
     }
     @RequestMapping(value = "test2",method = RequestMethod.POST)
