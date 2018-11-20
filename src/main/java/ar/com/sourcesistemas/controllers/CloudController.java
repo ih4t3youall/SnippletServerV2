060aec676da138f7402e4a42c9e84e3209166740
ih4t3youall <ih4t3youall@gmail.com>
2018-11-20 20:22:30 -0300

    syncro commit NO FUNCIONA

--- src/main/java/ar/com/sourcesistemas/controllers/CloudController.java
+++ src/main/java/ar/com/sourcesistemas/controllers/CloudController.java
@@ -1,13 +1,14 @@
 package ar.com.sourcesistemas.controllers;
 
 import ar.com.commons.send.domain.Category;
+import ar.com.commons.send.domain.Snipplet;
 import ar.com.commons.send.domain.User;
 import ar.com.commons.send.dto.CategoriaDTO;
 import ar.com.commons.send.dto.SendDTO;
 import ar.com.commons.send.dto.SnippletDTO;
+import ar.com.commons.send.parser.ConvertToDTOUtility;
 import ar.com.sourcesistemas.dao.CategoryDAO;
 import ar.com.sourcesistemas.dao.UserDAO;
-import ar.com.sourcesistemas.utilities.ConvertToDTOUtility;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,110 +18,174 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 public class CloudController {
 
-    final static Logger log = Logger.getLogger(CloudController.class);
-    private ObjectMapper mapper = new ObjectMapper();
+	final static Logger log = Logger.getLogger(CloudController.class);
+	private ObjectMapper mapper = new ObjectMapper();
 
+	@Autowired
+	private UserDAO userDao;
 
-    @Autowired
-    private UserDAO userDao;
+	@Autowired
+	private CategoryDAO categoryDAO;
 
-    @Autowired
-    private CategoryDAO categoryDAO;
+	private SendDTO getSendDTO(String jsonDTO) {
 
-    private SendDTO getSendDTO(String jsonDTO){
+		SendDTO sendDTO = null;
+		try {
+			sendDTO = mapper.readValue(jsonDTO, SendDTO.class);
+		} catch (Exception e) {
+			log.error("error de parseo: " + e.toString());
+			e.printStackTrace();
+		}
 
-        SendDTO sendDTO=null;
-        try {
-            log.info("parsing the string to sendDTO objet");
-            log.info("esto es nuevo");
-            sendDTO =mapper.readValue(jsonDTO,SendDTO.class);
-        } catch (Exception e) {
-            log.error("error de parseo: "+e.toString());
-            e.printStackTrace();
-        }
-        log.info("end parsing the string to sendDTO objet");
+		return sendDTO;
+	}
 
-        return sendDTO;
-    }
+	@RequestMapping(value = "returnCategory", method = RequestMethod.POST)
+	public String returnCategory(@RequestBody String jsonDTO) throws IOException {
+		log.info("Return category....");
+		SendDTO sendDTO = getSendDTO(jsonDTO);
+		User user = userDao.getUsernameByName(sendDTO.getUsername());
+		user.getCategory().stream().forEach(x -> log.info(x.getNombreCategoria()));
+		Category category = user.getCategory().stream()
+				.filter(x -> x.getNombreCategoria().equals(sendDTO.getCategoriaDTO().getNombre()))
+				.collect(Collectors.toList()).get(0);
+		CategoriaDTO categoriaDTO = ConvertToDTOUtility.fromCategoryToCategoryDTO(category);
+		log.info("Categoria a devolver: " + categoriaDTO.getNombre());
+		log.info("Snipplets a devolver:");
+		categoriaDTO.getSnipplets().stream().forEach(x -> log.info(x.getTitulo()));
+		sendDTO.setCategoriaDTO(categoriaDTO);
+		String result = mapper.writeValueAsString(sendDTO);
+		return result;
+	}
 
-    @RequestMapping(value ="returnCategory", method = RequestMethod.POST)
-    public String returnCategory(@RequestBody String jsonDTO) throws IOException {
-        log.info("Return category....");
-        SendDTO sendDTO = getSendDTO(jsonDTO);
-        User user = userDao.getUsernameByName(sendDTO.getUsername());
-        user.getCategory().stream().forEach(x -> log.info(x.getNombreCategoria()));
-        Category category = user.getCategory().stream().filter(x -> x.getNombreCategoria().equals(sendDTO.getCategoriaDTO().getNombre())).collect(Collectors.toList()).get(0);
-        CategoriaDTO categoriaDTO = ConvertToDTOUtility.fromCategoryToCategoryDTO(category);
-        log.info("Categoria a devolver: "+categoriaDTO.getNombre());
-        log.info("Snipplets a devolver:");
-        categoriaDTO.getSnipplets().stream().forEach(x -> log.info(x.getTitulo()));
-        sendDTO.setCategoriaDTO(categoriaDTO);
-        String result = mapper.writeValueAsString(sendDTO);
-        return result ;
-    }
+	@RequestMapping(value = "listarServer", method = RequestMethod.POST)
+	public String listarServer(@RequestBody String jsonDTO) {
+		String result = null;
+		log.info("jsondTO: " + jsonDTO);
+		log.info("listando servidor...");
+		SendDTO sendDTO = getSendDTO(jsonDTO);
+		User user = userDao.getUsernameByName(sendDTO.getUsername());
+		List<String> collect = user.getCategory().stream().map(s -> s.getNombreCategoria())
+				.collect(Collectors.toList());
+		log.info("categorias ...");
 
-    @RequestMapping(value = "listarServer", method = RequestMethod.POST)
-    public String listarServer(@RequestBody String jsonDTO) {
-        String result = null;
-        log.info("jsondTO: "+jsonDTO);
-        log.info("listando servidor...");
-        SendDTO sendDTO = getSendDTO(jsonDTO);
-        User user = userDao.getUsernameByName(sendDTO.getUsername());
-        List<String> collect = user.getCategory().stream().map(s -> s.getNombreCategoria()).collect(Collectors.toList());
-        log.info("categorias ...");
+		for (Category category : user.getCategory()) {
+			log.info(category.getNombreCategoria());
+		}
+		log.info("fin listado categorias");
 
-        for (Category category : user.getCategory() ){
-            log.info(category.getNombreCategoria());
-        }
-        log.info("fin listado categorias");
+		try {
+			result = mapper.writeValueAsString(collect);
+		} catch (IOException e) {
+			log.error("error en el writevalue as string");
+			e.printStackTrace();
+		}
+		log.info("result: " + result);
+		return result;
 
-        try {
-            result = mapper.writeValueAsString(collect);
-        } catch (IOException e) {
-            log.error("error en el writevalue as string");
-            e.printStackTrace();
-        }
-        log.info("result: "+result);
-        return result;
+	}
 
-    }
+	@RequestMapping(value = "guardarCategoria", method = RequestMethod.POST)
+	public String saveSnipplet(@RequestBody String jsonDTO) {
+		log.info("guardarCategoria ");
+		SendDTO sendDTO = null;
+		try {
+			sendDTO = getSendDTO(jsonDTO);
+		} catch (Exception e) {
+			log.info(e.toString());
+		}
+		User user = userDao.getUsernameByName(sendDTO.getUsername());
+		String nameCategoryDTO = sendDTO.getCategoriaDTO().getNombre();
+		List<Category> userCategories = user.getCategory().stream()
+				.filter(x -> x.getNombreCategoria().equals(nameCategoryDTO)).collect(Collectors.toList());
 
-    @RequestMapping(value = "guardarCategoria", method = RequestMethod.POST)
-    public String saveSnipplet(@RequestBody String jsonDTO ){
-        SendDTO sendDTO = getSendDTO(jsonDTO);
-        User user = userDao.getUsernameByName(sendDTO.getUsername());
+		Category userCategory = null;
+		List<SnippletDTO> toAdd = new LinkedList<SnippletDTO>();
+		if (userCategories.size() == 1) {
+			userCategory = userCategories.get(0);
+		} else {
+			for (SnippletDTO snipDTO : sendDTO.getCategoriaDTO().getSnipplets()) {
+				toAdd.add(snipDTO);
+			}
 
-        if (sendDTO != null) {
-            String categoriaNombre = sendDTO.getCategoriaDTO().getNombre();
-            List<SnippletDTO> snipplets = sendDTO.getCategoriaDTO().getSnipplets();
-            List<Category> category = user.getCategory().stream().filter(x -> x.getNombreCategoria().equals(categoriaNombre)).collect(Collectors.toList());
+		}
+		if (userCategory != null) {
+			mergeSnipplets(sendDTO, userCategory, toAdd);
+		} else {
+			addCategory(sendDTO.getCategoriaDTO(), user);
 
-            if(category.size() == 0){
-                Category categoria= ConvertToDTOUtility.convertTOCategory(sendDTO.getCategoriaDTO());
-                categoria.setUser(user);
-                user.addCategory(categoria);
-                categoryDAO.saveCategory(categoria);
-            }
+		}
 
-        }else {
+		for (SnippletDTO snip : toAdd) {
+			Snipplet snipplet = ConvertToDTOUtility.fromSnippletDTOtoSnippletWithCategory(snip, userCategory);
+			userCategory.add(snipplet);
+		}
+		categoryDAO.saveCategory(userCategory);
 
-            log.error("TheDTO was null");
-        }
+		return "hello";
+	}
 
+	private List<Category> addCategory(CategoriaDTO categoriaDTO, User user) {
+		List<Category> categories = null;
+		Category cat = new Category(categoriaDTO.getNombre(),user);
+		if (user.getCategory() == null) {
+			categories = new LinkedList<Category>();
+		} else {
+			categories = user.getCategory();
+		}
+		for (SnippletDTO snipDTO : categoriaDTO.getSnipplets()) {
+			Snipplet snip = ConvertToDTOUtility.fromSnippletDTOtoSnippletWithCategory(snipDTO, cat);
+			cat.add(snip);
 
-        return "hello ";
-    }
-    @RequestMapping(value = "test2",method = RequestMethod.POST)
-    public String test2(@RequestBody String json){
-        return "hello2 "+json;
-    }
+		}
+		categories.add(cat);
+		return categories;
 
+	}
 
+	@RequestMapping(value = "test2", method = RequestMethod.POST)
+	public String test2(@RequestBody String json) {
+		return "hello2 " + json;
+	}
+
+	public void mergeSnipplets(SendDTO sendDTO, Category userCategory, List<SnippletDTO> toAdd) {
+
+		log.info("Saving object");
+		boolean modify = false;
+		for (SnippletDTO sDTO : sendDTO.getCategoriaDTO().getSnipplets()) {
+
+			log.info("sDTO: " + sDTO.getTitulo());
+			for (Snipplet s : userCategory.getSnipplets()) {
+				log.info("snipplet: " + s.getTitulo());
+
+				if (sDTO.getTitulo().equals(s.getTitulo())) {
+
+					if (!sDTO.getContenido().equals(s.getContenido())) {
+
+						log.info("actualizando contenido");
+						s.setContenido(sDTO.getContenido());
+						modify = true;
+
+					}
+
+				}
+			}
+			if (!modify) {
+				log.info("fue modificado asi que hago el add");
+				toAdd.add(sDTO);
+				modify = !modify;
+				log.info("modify set to: " + modify);
+			}
+
+		}
+
+	}
 
 }
